acac09a46067e1b70d98a19eaba3f2abaf666b9c
Munish Prabhu <munish.paramanantham@appdynamics.com>
2021-12-23 23:37:37 +0530

    Updating teamcity settings file

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,42 +1,45 @@
-import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
+import jetbrains.buildServer.configs.kotlin.v2019_2.DslContext
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.project
+import jetbrains.buildServer.configs.kotlin.v2019_2.projectFeatures.buildReportTab
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
-
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
+import jetbrains.buildServer.configs.kotlin.v2019_2.version
 
 version = "2021.2"
 
 project {
-
     buildType(Cac)
+    features {
+        buildReportTab {
+            id = "PROJECT_EXT_4"
+            title = "Gradle Report"
+            startPage = "reports/tests/test/index.html"
+        }
+    }
 }
 
 object Cac : BuildType({
-    name = "CAC"
+    name = "GradlePlay CAC Test"
+    artifactRules = "e2e-tests/build/reports => reports"
 
     vcs {
         root(DslContext.settingsRoot)
+        cleanCheckout = true
     }
 
+    steps {
+        gradle {
+            name = "Execute Tests"
+            buildFile = "e2e-tests/build.gradle"
+            tasks = "clean test --tests com.demo.e2e.TestNGAnnotation"
+        }
+        script {
+            name = "Say GoodBye"
+            scriptContent = "echo 'Say GoodBye to Munishma'"
+        }
+    }
     triggers {
         vcs {
         }
