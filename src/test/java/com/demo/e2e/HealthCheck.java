83824b8411c935a959bf3a70ec71618fe0dc64a0
Munish Prabhu <munish.paramanantham@appdynamics.com>
2022-01-01 12:40:45 +0530

    Adding health check case

---
+++ src/test/java/com/demo/e2e/HealthCheck.java
@@ -0,0 +1,11 @@
+package com.demo.e2e;
+
+import org.testng.annotations.Test;
+
+public class HealthCheck {
+
+    @Test
+    public void checkApplicationStatus() {
+        System.out.println("Application is Live");
+    }
+}
