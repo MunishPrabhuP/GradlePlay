47f0a7d5ddc013f25cd1933d3870b524767fba05
admin <admin@TeamCity>
2022-01-01 11:23:35 +0530

    TeamCity change in 'AppD GradlePlay' project: general settings of 'Custom Test Runner' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -26,50 +26,22 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 version = "2021.2"
 
 project {
+
     buildType(CustomTestRunner)
-    features {
-        buildReportTab {
-            id = "PROJECT_EXT_1"
-            title = "Gradle Report"
-            startPage = "reports/tests/test/index.html"
-        }
-    }
 }
 
 object CustomTestRunner : BuildType({
     name = "Custom Test Runner"
+
     artifactRules = "e2e-tests/build/reports => reports"
 
-    params {
-        text(
-            "RUN_ONLY",
-            value = "",
-            label = "RUN_ONLY",
-            description = "To run Single Test : TestNGAnnotation/com.demo.e2e.TestNGAnnotation, To run all the Tests in a package - com.demo.e2e.*",
-            display = ParameterDisplay.PROMPT,
-            readOnly = false,
-            allowEmpty = false
-        )
-        select(
-            name = "TEST_TYPE",
-            value = "",
-            label = "TEST_TYPE",
-            description = "Type of Test(s)",
-            display = ParameterDisplay.PROMPT,
-            readOnly = false,
-            allowMultiple = false,
-            options = listOf("E2E-TESTS", "API-TESTS")
-        )
-    }
     vcs {
         root(DslContext.settingsRoot)
-        cleanCheckout = true
     }
 
     steps {
         gradle {
             tasks = "clean test --tests com.demo.e2e.SampleE2ETests"
-            name = "Execute Tests"
             buildFile = "e2e-tests/build.gradle"
         }
     }
