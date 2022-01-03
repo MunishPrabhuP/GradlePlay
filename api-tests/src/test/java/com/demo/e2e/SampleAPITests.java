4fb9f701d2e3fe23a25cca957600f14c93eebfa8
Munish Prabhu <munish.paramanantham@appdynamics.com>
2022-01-03 17:07:10 +0530

    Commit new code changes for PR Test and separate branch

--- api-tests/src/test/java/com/demo/e2e/SampleAPITests.java
+++ api-tests/src/test/java/com/demo/e2e/SampleAPITests.java
@@ -7,21 +7,21 @@ import org.testng.annotations.Test;
 public class SampleAPITests {
     @BeforeClass
     public void setUp() {
-        System.out.println("API Tests In SetUp");
+        System.out.println("API Tests In SetUp in Separate Branch");
     }
 
     @Test
     public void testcase1() {
-        System.out.println("API Tests In Test Case 1");
+        System.out.println("API Tests In Test Case 1 in Separate Branch");
     }
 
     @Test
     public void testcase2() {
-        System.out.println("API Tests In Test Case 2");
+        System.out.println("API Tests In Test Case 2 in Separate Branch");
     }
 
     @AfterClass
     public void tearDown() {
-        System.out.println("API Tests In Tear Down");
+        System.out.println("API Tests In Tear Down in Separate Branch");
     }
 }
--- e2e-tests/src/test/java/com/demo/e2e/SampleE2ETests.java
+++ e2e-tests/src/test/java/com/demo/e2e/SampleE2ETests.java
@@ -7,21 +7,21 @@ import org.testng.annotations.Test;
 public class SampleE2ETests {
     @BeforeClass
     public void setUp() {
-        System.out.println("E2E Tests In SetUp");
+        System.out.println("E2E Tests In SetUp in Separate Branch");
     }
 
     @Test
     public void testcase1() {
-        System.out.println("E2E Tests In Test Case 1");
+        System.out.println("E2E Tests In Test Case 1 in Separate Branch");
     }
 
     @Test
     public void testcase2() {
-        System.out.println("E2E Tests In Test Case 2");
+        System.out.println("E2E Tests In Test Case 2 in Separate Branch");
     }
 
     @AfterClass
     public void tearDown() {
-        System.out.println("E2E Tests In Tear Down");
+        System.out.println("E2E Tests In Tear Down in Separate Branch");
     }
 }
