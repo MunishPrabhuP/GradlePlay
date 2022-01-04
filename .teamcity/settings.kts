4c74f1209db8609c05a26b640b3fefb33879a516
Munish Prabhu <munish.paramanantham@appdynamics.com>
2022-01-04 23:32:48 +0530

    Applying latest Build Setting

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,44 +1,161 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
-import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
-
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.projectFeatures.buildReportTab
+import jetbrains.buildServer.configs.kotlin.v2019_2.ui.add
 
 version = "2020.1"
 
 project {
-
     buildType(HealthCheck)
+    buildType(E2ETests)
+    buildType(APITests)
+    buildType(CustomTestRunner)
+
+    features {
+        add {
+            buildReportTab {
+                id = "PROJECT_EXT_2"
+                title = "API Report"
+                startPage = "api-tests/reports/tests/test/index.html"
+            }
+        }
+        add {
+            buildReportTab {
+                id = "PROJECT_EXT_3"
+                title = "E2E Report"
+                startPage = "e2e-tests/reports/tests/test/index.html"
+            }
+        }
+        add {
+            buildReportTab {
+                id = "PROJECT_EXT_4"
+                title = "HealthCheck Report"
+                startPage = "health-check/reports/tests/test/index.html"
+            }
+        }
+    }
+    sequential {
+        buildType(HealthCheck)
+        parallel {
+            buildType(E2ETests)
+            buildType(APITests)
+        }
+    }
 }
 
 object HealthCheck : BuildType({
     name = "Health Check"
+    artifactRules = "library/build/reports/ => health-check/reports/"
 
     vcs {
         root(DslContext.settingsRoot)
+        cleanCheckout = true
     }
 
-    triggers {
-        vcs {
+    steps {
+        gradle {
+            name = "Execute Health Check(s)"
+            tasks = "clean test --tests com.demo.e2e.HealthCheck"
+            buildFile = "library/build.gradle"
+        }
+    }
+})
+
+object APITests : BuildType({
+    name = "API Tests"
+    artifactRules = "api-tests/build/reports/ => api-tests/reports/"
+
+    vcs {
+        root(DslContext.settingsRoot)
+        cleanCheckout = true
+    }
+    dependencies {
+        snapshot(HealthCheck) {
+            onDependencyFailure = FailureAction.FAIL_TO_START
+        }
+    }
+    steps {
+        gradle {
+            name = "Execute API Test(s)"
+            tasks = "clean test --tests com.demo.e2e.SampleAPITests"
+            buildFile = "api-tests/build.gradle"
+        }
+    }
+})
+
+object E2ETests : BuildType({
+    name = "E2E Tests"
+    artifactRules = "e2e-tests/build/reports/ => e2e-tests/reports/"
+
+    vcs {
+        root(DslContext.settingsRoot)
+        cleanCheckout = true
+    }
+    dependencies {
+        snapshot(HealthCheck) {
+            onDependencyFailure = FailureAction.FAIL_TO_START
+        }
+    }
+    steps {
+        gradle {
+            name = "Execute E2E Test(s)"
+            tasks = "clean test --tests com.demo.e2e.SampleE2ETests"
+            buildFile = "e2e-tests/build.gradle"
+        }
+    }
+})
+
+object CustomTestRunner : BuildType({
+    name = "Custom Test Runner"
+
+    vcs {
+        root(DslContext.settingsRoot)
+        cleanCheckout = true
+    }
+    params {
+        text(
+            name = "BRANCH",
+            value = "master",
+            label = "BRANCH",
+            description = "SCM/VCS Branch",
+            display = ParameterDisplay.PROMPT,
+            readOnly = false,
+            allowEmpty = false
+        )
+        text(
+            name = "RUN_ONLY",
+            value = "",
+            label = "RUN_ONLY",
+            description = "To run Single Test : SampleE2ETests/com.demo.e2e.SampleE2ETests, To run all the Tests in a package - com.demo.e2e.*",
+            display = ParameterDisplay.PROMPT,
+            readOnly = false,
+            allowEmpty = false
+        )
+        select(
+            name = "BUILD_FILE",
+            value = "",
+            label = "BUILD_FILE",
+            description = "Build File of the Test(s)",
+            display = ParameterDisplay.PROMPT,
+            readOnly = false,
+            allowMultiple = false,
+            options = listOf("api-tests/build.gradle", "e2e-tests/build.gradle", "library/build.gradle")
+        )
+    }
+    steps {
+        script {
+            name = "Fetching all Active Branch(s) from VCS"
+            scriptContent = "git fetch"
+        }
+        script {
+            name = "Checkout to Branch"
+            scriptContent = "git checkout %BRANCH%"
+        }
+        gradle {
+            name = "Execute Test(s)"
+            tasks = "clean test --tests %RUN_ONLY%"
+            buildFile = "%BUILD_FILE%"
         }
     }
 })
