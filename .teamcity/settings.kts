89a10b7837451ab49b1335f165be7693ee137ad4
Munish Prabhu <munish.paramanantham@appdynamics.com>
2022-01-24 23:24:02 +0530

    Reverting Param

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -56,19 +56,11 @@ object HealthCheck : BuildType({
         root(DslContext.settingsRoot)
         cleanCheckout = true
     }
-    params {
-        add {
-            param("env.TEAMCITY_BUILDCONF_NAME", "Health Check")
-        }
-    }
     steps {
         gradle {
             name = "Execute Health Check(s)"
             tasks = "clean test --tests com.demo.e2e.HealthCheck"
             buildFile = "library/build.gradle"
-        }
-        script {
-
         }
     }
 })
