4aefcfd9223a505f8034e87e4f0a36c1f279923d
Munish Prabhu <munish.paramanantham@appdynamics.com>
2022-01-25 13:20:48 +0530

    Displaying Run Mode

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -139,7 +139,7 @@ object E2ETests : BuildType({
             buildFile = "e2e-tests/build.gradle"
         }
         script {
-            scriptContent = """echo "##teamcity[setParameter name='env.TEAMCITY_BUILDCONF_NAME' value='E2E Tests']""""
+            scriptContent = """echo "##teamcity[setParameter name='env.TEAMCITY_BUILDCONF_NAME' value='E2E %RUN_MODE% Tests']""""
         }
         script {
             scriptContent = """echo "Executing %env.TEAMCITY_BUILDCONF_NAME% suite""""
