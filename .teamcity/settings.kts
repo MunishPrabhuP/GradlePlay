991baba247e1e9a3cba4333318a8345940845ebb
Munish Prabhu <munish.paramanantham@appdynamics.com>
2022-02-23 12:50:30 +0530

    UI Test Cleanup

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,3 @@
-import jetbrains.buildServer.configs.kotlin.v2019_2.BuildStep
 import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
 import jetbrains.buildServer.configs.kotlin.v2019_2.DslContext
 import jetbrains.buildServer.configs.kotlin.v2019_2.FailureAction
@@ -17,7 +16,6 @@ project {
     buildType(HealthCheck)
     buildType(E2ETests)
     buildType(APITests)
-    buildType(UITests)
     buildType(Release)
     buildType(ReleaseCycleSetup)
 
@@ -50,7 +48,6 @@ project {
         parallel {
             buildType(E2ETests)
             buildType(APITests)
-//            buildType(UITests)
         }
         buildType(Release)
     }
@@ -168,47 +165,6 @@ object Release : BuildType({
     steps {
         script {
             scriptContent = """echo "Executing %reverse.dep.*.RELEASE_RUN_MODE% suite""""
-        }
-    }
-})
-
-object UITests : BuildType({
-    name = "UI Tests"
-    artifactRules = "ui-test-report.xml => ui-test-report.xml"
-
-    params {
-        text(
-            name = "TestIM_BRANCH",
-            value = "feature/lic-usage",
-            label = "TestIM BRANCH",
-            description = "Feature Branch in TestIM",
-            display = ParameterDisplay.NORMAL,
-            readOnly = false,
-            allowEmpty = false
-        )
-    }
-    steps {
-        script {
-            clearConditions()
-            scriptContent =
-                """
-                set -x
-                mkdir -p "%system.teamcity.build.workingDir%/.npm-packages"
-                prefix=%system.teamcity.build.workingDir%/.npm-packages
-                NPM_PACKAGES="%system.teamcity.build.workingDir%/.npm-packages"
-                export PATH="${'$'}PATH:${'$'}NPM_PACKAGES/bin"
-                export NODE_PATH="${'$'}NODE_PATH:${'$'}NPM_PACKAGES/lib/node_modules"
-                npm config set prefix %system.teamcity.build.workingDir%/.npm-packages
-                npm install -g @testim/testim-cli
-                set +x
-                %system.teamcity.build.workingDir%/.npm-packages/bin/testim \
-                  --token "b0Q13JwYtxAQ7EecdNMLbkW4YE61DcUYkpe1oAAQCTYjwwbWYA" \
-                  --project "aeHu7B27U7VgxRvjagV2" \
-                  --label "Licensing" \
-                  --branch %TestIM_BRANCH% \
-                  --grid "Testim-Grid" \
-                  --reporters teamcity,console
-            """.trimIndent()
         }
     }
 })
