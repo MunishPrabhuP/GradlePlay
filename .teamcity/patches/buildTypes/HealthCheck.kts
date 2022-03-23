010d2d35d6b52f967d60f4432de50405b0808fe5
admin <>
2022-03-23 16:51:14 +0530

    TeamCity change in 'IdeaImplementation' project: 'Health Check' build configuration settings were updated

--- .teamcity/patches/buildTypes/HealthCheck.kts
+++ .teamcity/patches/buildTypes/HealthCheck.kts
@@ -21,6 +21,7 @@ changeBuildType(RelativeId("HealthCheck")) {
     steps {
         insert(1) {
             script {
+                enabled = false
                 scriptContent = """
                     set ENVIRONMENT=Alameda
                     echo %%ENVIRONMENT%%
