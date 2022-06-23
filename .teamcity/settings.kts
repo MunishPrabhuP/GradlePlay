f55ddab45585affa9aab5eaa76de947684a2b59b
Munish Prabhu <munish.paramanantham@appdynamics.com>
2022-06-23 23:38:30 +0530

    Cleaning Health Check issue

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -69,11 +69,6 @@ object HealthCheck : BuildType({
             tasks = "clean test --tests com.demo.e2e.HealthCheck"
             buildFile = "library/build.gradle"
         }
-        script {
-            name = "Checking variable assignment"
-            val ENVIRONMENT = "Diablo-Master"
-            scriptContent = "echo %ENVIRONMENT%"
-        }
     }
 })
 
